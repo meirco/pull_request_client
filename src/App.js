d5bab36b25ede1917c3eb7f0cc569fe875edd97d
meirco <meirco26@gmail.com>
2021-06-20 13:17:44 +0300

    push the code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import MUIDataTable from "mui-datatables";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    getPullData()
+  }, [])
+
+  const getPullData = () => {
+    fetch('http://localhost:5000/pulldata')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          setData([data])
+        })
+  }
+
+  const columns = ["action", "number", "pull_request", "raw_data", "repository", "sender"];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <button onClick={getPullData}>Get data</button>
+      </div>
+      <MUIDataTable
+        title={"Last pull request data"}
+        data={data ? data : []}
+        columns={columns}
+        style={{verticalAlign: "top"}}
+        options={{
+            selectableRows: "none",
+            pagination: false,
+            print: false,
+            filter: false,
+            download: false,
+            viewColumns: false,
+        }}
+    />
     </div>
   );
 }
